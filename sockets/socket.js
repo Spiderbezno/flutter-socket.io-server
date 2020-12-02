8915212eace41399377f900ce2c0a70e647ff33b
Jorge <jorge.gomes@andorratelecom.ad>
2020-12-02 20:40:02 +0100

    Canvios sobre bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,13 +1,49 @@
-const {io} = require('../index')
+const { io } = require('../index')
+const Bands = require('../models/bands')
+const Band = require('../models/band')
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Héroes del Silencio'));
+bands.addBand(new Band('Metallica'));
+
 // Mensajes de Sockets
 io.on('connection', client => {
-    console.log('Cliente conectado');
-    client.on('disconnect', () => { 
-        console.log('Cliente desconectado');
-     });
+   console.log('Cliente conectado');
 
-     client.on('mensaje', (payload) => {
-        console.log('Mensaje!!', payload);
-        io.emit('mensaje', { admin: 'Nuevo mensaje'});
-     });
-  });
+   client.emit('active-bands', bands.getBands());
+
+   client.on('disconnect', () => {
+      console.log('Cliente desconectado');
+   });
+
+   client.on('mensaje', (payload) => {
+      console.log('Mensaje!!', payload);
+      io.emit('mensaje', { admin: 'Nuevo mensaje' });
+   });
+
+   client.on('emitir-mensaje', (payload) => {
+      // io.emit('nuevo-mensaje', payload); emitir a todo el mundo
+      client.broadcast.emit('nuevo-mensaje', payload); // emite a todos mesno al que lo emitió
+   });
+
+   client.on('vote-band', (payload) => {
+      bands.voteBand(payload.id);
+      // io.emit('nuevo-mensaje', payload); emitir a todo el mundo
+      io.emit('active-bands', bands.getBands());
+   });
+
+   client.on('add-band', (payload) => {
+      bands.addBand(new Band(payload.name));
+      // io.emit('nuevo-mensaje', payload); emitir a todo el mundo
+      io.emit('active-bands', bands.getBands());
+   });
+
+   client.on('delete-band', (payload) => {
+      bands.deleteBand(payload.id)
+      // io.emit('nuevo-mensaje', payload); emitir a todo el mundo
+      io.emit('active-bands', bands.getBands());
+   });
+});
